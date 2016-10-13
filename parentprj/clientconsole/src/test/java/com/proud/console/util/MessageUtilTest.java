68fa6d9540afa58e60e2525df29caaf1ad78d9ce
Fabrizio Faustinoni <fabry_00@hotmail.com>
2016-10-13 18:27:24 +0200

    fixed junit tests

--- parentprj/clientconsole/src/test/java/com/proud/console/util/MessageUtilTest.java
+++ parentprj/clientconsole/src/test/java/com/proud/console/util/MessageUtilTest.java
@@ -14,9 +14,10 @@ public class MessageUtilTest {
     @Test
     public void getMsg() throws Exception {
         String msg = "Message";
-        DateUtils util = new DateUtils();
-        String expected = "[ " + util.getNow() + " ] " + msg;
-        assertEquals(expected,new MessageUtil().getMsg(msg));
+        String retMsg = new MessageUtil().getMsg(msg);
+        assertTrue(retMsg.contains("["));
+        assertTrue(retMsg.contains("]"));
+        assertTrue(retMsg.contains(msg));
     }
 
 }
